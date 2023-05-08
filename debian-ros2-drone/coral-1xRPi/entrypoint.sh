b843532d292785b080d7ca6274a46372da0b4f7a
aadne <aadne.kvaale@gmail.com>
2023-05-09 00:38:36 +0200

    Integrated img_processing package
    Integrated the 'img_processing' ROS2 package from 'https://github.com/Aerial-Edge/ros2-image-processing-coral' into the existing codebase.

--- debian-ros2-drone/coral-1xRPi/entrypoint.sh
+++ debian-ros2-drone/coral-1xRPi/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source install/setup.bash
-(trap 'kill 0' SIGINT; ros2 run drone_pkg autopilot_node & ros2 run drone_controller controller_node & wait)
+(trap 'kill 0' SIGINT; ros2 run drone_pkg autopilot_node & ros2 run drone_controller controller_node & ros2 run img_processing videostream_node & ros2 run img_processing detect_node & wait)
