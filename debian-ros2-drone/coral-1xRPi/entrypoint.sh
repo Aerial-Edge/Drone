cc166762629e094b1ae8b8af2b1c9b1558100ed9
aadne <aadne.kvaale@gmail.com>
2023-05-10 19:50:41 +0200

    Fixed camera passthrough
    Fixed so camera device is correctly passed through to container

--- debian-ros2-drone/coral-1xRPi/entrypoint.sh
+++ debian-ros2-drone/coral-1xRPi/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source install/setup.bash
+sudo udevadm control --reload
 (trap 'kill 0' SIGINT; ros2 run drone_pkg autopilot_node & ros2 run drone_controller controller_node & ros2 run img_processing videostream_node & ros2 run img_processing detect_node & wait)
